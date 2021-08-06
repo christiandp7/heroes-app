7d309819104415b30050f4fb3d8bab7248f4a04d
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-05 22:08:01 -0400

    :white_check_mark: Add Navbar test

---
+++ src/tests/components/Navbar.test.js
@@ -0,0 +1,47 @@
+import { mount } from 'enzyme'
+import { Router, MemoryRouter } from 'react-router-dom'
+import { AuthContext } from '../../auth/AuthContext'
+import Navbar from '../../components/ui/Navbar'
+import { types } from '../../types/types'
+
+describe('<Navbar /> tests', () => {
+	const historyMock = {
+		push: jest.fn(),
+		replace: jest.fn(),
+		location: {},
+		listen: jest.fn(),
+		createHref: jest.fn(),
+	}
+	const contextValue = {
+		dispatch: jest.fn(),
+		user: {
+			logged: true,
+			name: 'Juan',
+		},
+	}
+	const wrapper = mount(
+		<AuthContext.Provider value={contextValue}>
+			<MemoryRouter>
+				<Router history={historyMock}>
+					<Navbar />
+				</Router>
+			</MemoryRouter>
+		</AuthContext.Provider>,
+	)
+
+	afterEach(() => {
+		jest.clearAllMocks()
+	})
+
+	test('should be shown correctly', () => {
+		expect(wrapper).toMatchSnapshot()
+		expect(wrapper.find('.text-info').text().trim()).toBe('Juan')
+	})
+	test('should call logout and use history', () => {
+		wrapper.find('button').prop('onClick')()
+		expect(contextValue.dispatch).toHaveBeenCalledWith({
+			type: types.logout,
+		})
+		expect(historyMock.replace).toHaveBeenCalledWith('/login')
+	})
+})
